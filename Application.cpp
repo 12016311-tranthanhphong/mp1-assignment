e8f4f3e2fbba63dfbc282a60539813ef1fdf83ec
tranthanhphong <tranthanhphonglhcd@gmail.com>
2015-11-01 05:36:46 +0000

    Update Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -1,19 +1,24 @@
+
 /**********************************
 * FILE NAME: Application.cpp
 *
-* DESCRIPTION: Application layer class function definitions
-**********************************/
+* DESCRIPTION: Application layer class function definitions*******************/
+
 #include "Application.h"
+
 void handler(int sig) {
 void *array[10];
 size_t size;
+
 // get void*'s for all entries on the stack
 size = backtrace(array, 10);
+
 // print out all the frames to stderr
 fprintf(stderr, "Error: signal %d:\n", sig);
 backtrace_symbols_fd(array, size, STDERR_FILENO);
 exit(1);
 }
+
 /**********************************
 * FUNCTION NAME: main
 *
@@ -25,14 +30,17 @@ if ( argc != ARGS_COUNT ) {
 cout<<"Configuration (i.e., *.conf) file File Required"<<endl;
 return FAILURE;
 }
+
 // Create a new application object
 Application *app = new Application(argv[1]);
 // Call the run function
 app->run();
 // When done delete the application object
 delete(app);
+
 return SUCCESS;
 }
+
 /**
 * Constructor of the Application class
 */
@@ -44,6 +52,7 @@ par->setparams(infile);
 log = new Log(par);
 en = new EmulNet(par);
 mp1 = (MP1Node **) malloc(par->EN_GPSZ * sizeof(MP1Node *));
+
 /*
 * Init all nodes
 */
@@ -59,6 +68,7 @@ log->LOG(&(mp1[i]->getMemberNode()->addr), "APP");
 delete addressOfMemberNode;
 }
 }
+
 /**
 * Destructor
 */
@@ -71,6 +81,7 @@ delete mp1[i];
 free(mp1);
 delete par;
 }
+
 /**
 * FUNCTION NAME: run
 *
@@ -83,6 +94,7 @@ int timeWhenAllNodesHaveJoined = 0;
 // boolean indicating if all nodes have joined
 bool allNodesJoined = false;
 srand(time(NULL));
+
 // As time runs along
 for( par->globaltime = 0; par->globaltime < TOTAL_RUNNING_TIME; ++par->globaltime ) {
 // Run the membership protocol
@@ -90,13 +102,17 @@ mp1Run();
 // Fail some nodes
 fail();
 }
+
 // Clean up
 en->ENcleanup();
+
 for(i=0;i<=par->EN_GPSZ-1;i++) {
 mp1[i]->finishUpThisNode();
 }
+
 return SUCCESS;
 }
+
 /**
 * FUNCTION NAME: mp1Run
 *
@@ -104,8 +120,10 @@ return SUCCESS;
 */
 void Application::mp1Run() {
 int i;
+
 // For all the nodes in the system
 for( i = 0; i <= par->EN_GPSZ-1; i++) {
+  
 /*
 * Receive messages from the network and queue them in the membership protocol queue
 */
@@ -113,9 +131,12 @@ if( par->getcurrtime() > (int)(par->STEP_RATE*i) && !(mp1[i]->getMemberNode()->b
 // Receive messages from the network and queue them
 mp1[i]->recvLoop();
 }
+
 }
+
 // For all the nodes in the system
 for( i = par->EN_GPSZ - 1; i >= 0; i-- ) {
+  
 /*
 * Introduce nodes into the distributed system
 */
@@ -125,6 +146,7 @@ mp1[i]->nodeStart(JOINADDR, par->PORTNUM);
 cout<<i<<"-th introduced node is assigned with the address: "<<mp1[i]->getMemberNode()->addr.getAddress() << endl;
 nodeCount += i;
 }
+
 /*
 * Handle all the messages in your queue and send heartbeats
 */
@@ -137,8 +159,10 @@ log->LOG(&mp1[i]->getMemberNode()->addr, "@@time=%d", par->getcurrtime());
 }
 #endif
 }
+
 }
 }
+
 /**
 * FUNCTION NAME: fail
 *
@@ -148,10 +172,12 @@ log->LOG(&mp1[i]->getMemberNode()->addr, "@@time=%d", par->getcurrtime());
 */
 void Application::fail() {
 int i, removed;
+
 // fail half the members at time t=400
 if( par->DROP_MSG && par->getcurrtime() == 50 ) {
 par->dropmsg = 1;
 }
+
 if( par->SINGLE_FAILURE && par->getcurrtime() == 100 ) {
 removed = (rand() % par->EN_GPSZ);
 #ifdef DEBUGLOG
@@ -168,10 +194,13 @@ log->LOG(&mp1[i]->getMemberNode()->addr, "Node failed at time = %d", par->getcur
 mp1[i]->getMemberNode()->bFailed = true;
 }
 }
+
 if( par->DROP_MSG && par->getcurrtime() == 300) {
 par->dropmsg=0;
 }
+
 }
+
 /**
 * FUNCTION NAME: getjoinaddr
 *
@@ -184,5 +213,5 @@ joinaddr.init();
 *(int *)(&(joinaddr.addr))=1;
 *(short *)(&(joinaddr.addr[4]))=0;
 //trace.funcExit("Application::getjoinaddr", SUCCESS);
-return joinaddr;
+return joinaddr
 }

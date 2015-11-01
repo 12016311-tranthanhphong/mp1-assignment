0c50ca7931119714bb86953152f79ed091e03bb4
tranthanhphong <tranthanhphonglhcd@gmail.com>
2015-11-01 05:47:57 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ sudo mkdir grade-dir
 cd grade-dir
 wget https://spark-public.s3.amazonaws.com/cloudcomputing/assignments/mp1/mp1.zip || { echo 'ERROR ... Please install wget'; exit 1; }
 unzip mp1.zip || { echo 'ERROR ... Zip file not found' ; exit 1; }
-#cd mp1
+cd mp1
 cp ../../MP1Node.* .
 make clean > /dev/null
 make > /dev/null

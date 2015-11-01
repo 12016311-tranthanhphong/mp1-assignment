9cf3335e84bdec3dee129a2aaba4f2b27208d198
tranthanhphong <tranthanhphonglhcd@gmail.com>
2015-11-01 05:59:00 +0000

    be

--- Params.h
+++ Params.h
@@ -1,50 +1,39 @@
 /**********************************
- * FILE NAME: Params.cpp
+ * FILE NAME: Params.h
  *
- * DESCRIPTION: Definition of Parameter class
+ * DESCRIPTION: Header file of Parameter class
  **********************************/
 
+#ifndef _PARAMS_H_
+#define _PARAMS_H_
+
+#include "stdincludes.h"
 #include "Params.h"
+#include "Member.h"
+
+enum testTYPE { CREATE_TEST, READ_TEST, UPDATE_TEST, DELETE_TEST };
 
 /**
- * Constructor
- */
-Params::Params(): PORTNUM(8001) {}
-
-/**
- * FUNCTION NAME: setparams
+ * CLASS NAME: Params
  *
- * DESCRIPTION: Set the parameters for this test case
+ * DESCRIPTION: Params class describing the test cases
  */
-void Params::setparams(char *config_file) {
-	FILE *fp = fopen(config_file,"r");
+class Params{
+public:
+	int MAX_NNB;                // max number of neighbors
+	int SINGLE_FAILURE;			// single/multi failure
+	double MSG_DROP_PROB;		// message drop probability
+	double STEP_RATE;		    // dictates the rate of insertion
+	int EN_GPSZ;			    // actual number of peers
+	int MAX_MSG_SIZE;
+	int DROP_MSG;
+	int dropmsg;
+	int globaltime;
+	int allNodesJoined;
+	short PORTNUM;
+	Params();
+	void setparams(char *);
+	int getcurrtime();
+};
 
-	fscanf(fp,"MAX_NNB: %d", &MAX_NNB);
-	fscanf(fp,"\nSINGLE_FAILURE: %d", &SINGLE_FAILURE);
-	fscanf(fp,"\nDROP_MSG: %d", &DROP_MSG);
-	fscanf(fp,"\nMSG_DROP_PROB: %lf", &MSG_DROP_PROB);
-
-	//printf("Parameters of the test case: %d %d %d %lf\n", MAX_NNB, SINGLE_FAILURE, DROP_MSG, MSG_DROP_PROB);
-
-	EN_GPSZ = MAX_NNB;
-	STEP_RATE=.25;
-	MAX_MSG_SIZE = 4000;
-	globaltime = 0;
-	dropmsg = 0;
-	allNodesJoined = 0;
-	for ( unsigned int i = 0; i < EN_GPSZ; i++ ) {
-		allNodesJoined += i;
-	}
-	fclose(fp);
-	return;
-}
-
-/**
- * FUNCTION NAME: getcurrtime
- *
- * DESCRIPTION: Return time since start of program, in time units.
- * 				For a 'real' implementation, this return time would be the UTC time.
- */
-int Params::getcurrtime(){
-    return globaltime;
-}
+#endif /* _PARAMS_H_ */
